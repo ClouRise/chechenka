79693bec367ed87497ebbf45446279dae07eb181
Boris <mr.clourise@gmail.com>
2024-10-14 11:06:05 +0300

    map: new url and map

--- djChechenka/couriermap/urls.py
+++ djChechenka/couriermap/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.ex),
+    path('map/', views.map),
 ]
--- djChechenka/couriermap/views.py
+++ djChechenka/couriermap/views.py
@@ -2,3 +2,5 @@ from django.shortcuts import render
 
 def ex(request):
     return render(request, 'couriermap/index.html')
+def map(request):
+    return render(request, 'couriermap/map.html')
